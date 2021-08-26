37a617fdbfeda85d0d5c5e8d8420820ad9903acd
AndresC-O <62961669+AndresC-O@users.noreply.github.com>
2021-08-25 20:07:21 -0600

    Update script.js

--- CourseJavaScript/Scripts/script.js
+++ CourseJavaScript/Scripts/script.js
@@ -97,3 +97,74 @@
 // console.log(Math.random()*(10 -5) + 5);//numero entre 5 y 10
 // console.log(Math.sign(numero));//Devuelve el signo del numero
 // console.log(Math.sqrt(9));//devuelve raiz cuadrada de x
+
+//Clase #7--------------------------------------------
+//CONDICIONALES
+
+// let num = 0;
+// let num1 = 0;
+// let num2 = 0;
+
+// if (num > 0){
+//     console.log(`${num} es mayor que 0.`);
+// }
+// else if(num < 0){
+//     console.log(`${num} es menor que 0.`);
+// }
+// else{
+//     console.log(`${num} es igual que 0.`);
+// }
+
+// Operadores Logicos
+// if(num1 > 0){
+//     if(num2 > 0){
+//         console.log(`${num1} y ${num2} son mayores que 0`);
+//     }
+//     else if(num2 < 0){
+//         console.log(`${num1} es mayor que 0 y ${num2} es menor que 0`);
+//     }
+//     else{
+//         console.log(`${num1} es mayor que 0 y ${num2} es igual que 0`);
+//     }
+// }
+// else if(num1 < 0){
+//     if(num2 > 0){
+//         console.log(`${num1} no es mayor que 0 y ${num2} es mayor que 0`);
+//     }
+//     else if(num2 < 0){
+//         console.log(`${num1}  y ${num2} son menores que 0`);
+//     }
+//     else{
+//         console.log(`${num1} es menor que 0 y ${num2} es igual que 0`);
+//     }
+// }
+// else{
+//     if(num2 > 0){
+//         console.log(`${num1} es igual que 0 y ${num2} es mayor que 0`);
+//     }
+//     else if(num2 < 0){
+//         console.log(`${num1} es igual que 0 y ${num2} es menor que 0`);
+//     }
+//     else{
+//         console.log(`${num1} y ${num2} son iguales que 0`);
+//     }
+// }
+
+// let word = 'ho';
+// if(word.length > 4){
+//     console.log(`${word} tiene mas de 4 letras`);
+// }
+// else if(word.length < 4){
+//     console.log(`${word} tiene menos de 4 letras`);
+// }
+// else{
+//     console.log(`${word} tiene 4 letras`);
+// }
+
+// let respuesta = false;
+// if(respuesta == true){
+//     console.log(`Respuesta tiene el valor true`);
+// }
+// else if(respuesta == false){
+//     console.log(`Respuesta tiene el valor false`);
+// }
